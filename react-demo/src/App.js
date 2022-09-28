1a2e065b3c493d7acf58f602e2965ac2bd62fd20
Geovanny Silva Ribeiro <geovannysilvaribeiro@ip-172-27-242-206.ec2.internal>
2022-09-28 18:48:44 -0300

    Added button as example

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import {Greet} from './components/Greet';
+import { Greet } from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Bruce' heroName = 'Batman' />
-      <Greet name='Clark' heroName = 'Superman'/>
-      <Greet name='Diana' heroName = 'Wonder Woman'>
+      <Greet name='Bruce' heroName='Batman' />
+      <Greet name='Clark' heroName='Superman'>
+        <button>Action</button>
+      </Greet>
+      <Greet name='Diana' heroName='Wonder Woman'>
         <p>This is children props</p>
       </Greet>
     </div>
