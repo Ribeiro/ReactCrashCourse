d4d54269e61c09a1411f805f6b70bdd5a87717ec
Geovanny Silva Ribeiro <geovannysilvaribeiro@ip-172-27-243-59.ec2.internal>
2022-09-29 12:39:18 -0300

    Ending state example implementation

--- react-demo/src/components/Message.js
+++ react-demo/src/components/Message.js
@@ -1,6 +1,11 @@
-export const Message = (props) => {
-    console.log(props)
+import { useState } from 'react'
+
+export const Message = () => {
+    const [message, setMessage] = useState('Welcome visitor')
     return (
-        <h1>Welcome visitor</h1>
+        <div>
+            <h1>{message}</h1>
+            <button onClick={() => setMessage('Thank you for subscribing!')}>Subscribe</button>
+        </div>
     )
 }
