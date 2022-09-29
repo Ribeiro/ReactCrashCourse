15d5f0d088b62c3740e2e25409afed4045d040f8
Geovanny Silva Ribeiro <geovannysilvaribeiro@ip-172-27-243-59.ec2.internal>
2022-09-29 12:31:47 -0300

    Start using state (mutable) instead of props (immutable)

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Message } from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Bruce' heroName='Batman' />
+      {/* <Greet name='Bruce' heroName='Batman' />
       <Greet name='Clark' heroName='Superman'>
         <button>Action</button>
       </Greet>
       <Greet name='Diana' heroName='Wonder Woman'>
         <p>This is children props</p>
-      </Greet>
+      </Greet> */}
+      <Message />
     </div>
   );
 }
---
+++ react-demo/src/components/Message.js
@@ -0,0 +1,6 @@
+export const Message = (props) => {
+    console.log(props)
+    return (
+        <h1>Welcome visitor</h1>
+    )
+}
