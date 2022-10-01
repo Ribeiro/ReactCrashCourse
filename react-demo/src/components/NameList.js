514b544e9d57c60ceb0a6fb48a9d7afbc1c7313f
Geovanny Silva Ribeiro <geovannysilvaribeiro@192.168.1.6>
2022-10-01 11:12:47 -0300

    Unique prop fix using uuid on List rendering

--- react-demo/src/components/NameList.js
+++ react-demo/src/components/NameList.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import uuid from 'react-uuid';
 export const NameList = () => {
     const names = ['Bruce', 'Clark', 'Diana']
     return <div>
         {
             names.map((name) => {
-                return <h2>{name}</h2>
+                return <h2 key={uuid()}>{name}</h2>
             })
         }
     </div>
