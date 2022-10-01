95def589dbd6046f838ba45bf721eded297f1202
Geovanny Silva Ribeiro <geovannysilvaribeiro@192.168.1.6>
2022-10-01 10:46:19 -0300

    Passing argument from Child to Parent

--- react-demo/src/components/ChildComponent.js
+++ react-demo/src/components/ChildComponent.js
@@ -1,6 +1,6 @@
 export const ChildComponent = (props) => {
     return <div>
-        <button onClick={props.greetHandler}>Greet Parent</button>
+        <button onClick={() => props.greetHandler('child')}>Greet Parent</button>
     </div>
 
 }
--- react-demo/src/components/ParentComponent.js
+++ react-demo/src/components/ParentComponent.js
@@ -1,8 +1,8 @@
 import { ChildComponent } from "./ChildComponent"
 
 export const ParentComponent = () => {
-    const greetParent = () => {
-        alert('Hello parent')
+    const greetParent = (childName) => {
+        alert(`Hello parent ${childName}`)
 
     }
 
